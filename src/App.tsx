fb8085b9da55db9fb0f5833f2b775ec54c63a958
Peter O'Leary <pete@timelight.com>
2022-02-18 16:43:28 -0800

    First checkin: LinkedIn callback is working

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import {
+    LedgerWalletAdapter,
+    PhantomWalletAdapter,
+    SlopeWalletAdapter,
+    SolflareWalletAdapter,
+    SolletExtensionWalletAdapter,
+    SolletWalletAdapter,
+    TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import React, { FC, ReactNode, useMemo } from 'react';
+
+import LinkedInPage from "./LinkedInPage.jsx";
+import { LinkedInCallback } from "react-linkedin-login-oauth2";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const App: FC = () => {
+
+    return (
+        <Router>
+        <Routes>
+            <Route path="/test" element={<span >Inline works</span>} />
+            <Route path="/linkedin" element={<LinkedInCallback />} />
+            <Route path="/" element={<Body />} />
+        </Routes>
+      </Router>
+    );
+  }
+
+const Body: FC = () => {
+    return (
+        <Context>
+            <Content />
+        </Context>
+    );
+};
+
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded.
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new SlopeWalletAdapter(),
+            new SolflareWalletAdapter({ network }),
+            new TorusWalletAdapter(),
+            new LedgerWalletAdapter(),
+            new SolletWalletAdapter({ network }),
+            new SolletExtensionWalletAdapter({ network }),
+        ],
+        [network]
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+const Content: FC = () => {
+    return ( <div>
+        <WalletMultiButton />
+        <LinkedInPage />
+        </div>
+        );
+};
+
+export default App
